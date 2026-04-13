bc152cf77cb2bff6f4084174f10bc6e0328b9b52
Joseph A. Blessman <jblessman24@msu.idserve.net>
2026-04-13 08:16:32 -0400

    Initial Commit

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Joseph Allen Blessman on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PortfolioAppTests/PortfolioAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  PortfolioAppTests.swift
+//  PortfolioAppTests
+//
+//  Created by Joseph Allen Blessman on 4/13/26.
+//
+
+import Testing
+@testable import PortfolioApp
+
+struct PortfolioAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
